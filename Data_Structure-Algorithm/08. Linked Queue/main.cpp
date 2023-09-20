b173784a86e6329c2ec04ef2514bb2c1c94aa729
Chosungwoo123 <ejyoo12345@naver.com>
2023-09-20 21:35:47 +0900

    Make a Linked Queu.

---
+++ Data_Structure-Algorithm/08. Linked Queue/main.cpp
@@ -0,0 +1,31 @@
+// C언어로 구현한 링크드 리스트를 이용한 큐
+
+#include "LinkedQueue.h"
+
+int main(int argc, char* argv[])
+{
+	Node* Popped;
+	LinkedQueue* Queue;
+
+	LQ_CreateQueue(&Queue);
+
+	LQ_Enqueue(Queue, LQ_CreateNode("abc"));
+	LQ_Enqueue(Queue, LQ_CreateNode("def"));
+	LQ_Enqueue(Queue, LQ_CreateNode("efg"));
+	LQ_Enqueue(Queue, LQ_CreateNode("hij"));
+
+	printf("Queue Size : %d\n", Queue->Count);
+
+	while (LQ_IsEmpty(Queue) == 0)
+	{
+		Popped = LQ_Dequeue(Queue);
+
+		printf("Dequeue : %s\n", Popped->Data);
+
+		LQ_DestroyNode(Popped);
+	}
+
+	LQ_DestroyQueue(Queue);
+
+	return 0;
+}
