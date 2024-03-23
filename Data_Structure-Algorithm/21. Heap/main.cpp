41db550136184c18f41300802bbd37bc8a2c2690
Chosungwoo123 <ejyoo12345@naver.com>
2024-03-23 23:01:59 +0900

    Make a Heap.

---
+++ Data_Structure-Algorithm/21. Heap/main.cpp
@@ -0,0 +1,37 @@
+// C언어로 구현한 힙
+
+#include "Heap.h"
+
+int main(int argc, char* argv[])
+{
+	Heap* H = HEAP_Create(3);
+	HeapNode MinNode;
+
+	HEAP_Insert(H, 12);
+	HEAP_Insert(H, 87);
+	HEAP_Insert(H, 111);
+	HEAP_Insert(H, 34);
+	HEAP_Insert(H, 16);
+	HEAP_Insert(H, 75);
+	HEAP_PrintNodes(H);
+
+	HEAP_DeleteMin(H, &MinNode);
+	HEAP_PrintNodes(H);
+
+	HEAP_DeleteMin(H, &MinNode);
+	HEAP_PrintNodes(H);
+
+	HEAP_DeleteMin(H, &MinNode);
+	HEAP_PrintNodes(H);
+
+	HEAP_DeleteMin(H, &MinNode);
+	HEAP_PrintNodes(H);
+
+	HEAP_DeleteMin(H, &MinNode);
+	HEAP_PrintNodes(H);
+
+	HEAP_DeleteMin(H, &MinNode);
+	HEAP_PrintNodes(H);
+
+	return 0;
+}
