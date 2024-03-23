53999e82e098737bef439710ce9dcdac46b97478
Chosungwoo123 <ejyoo12345@naver.com>
2024-03-23 19:39:33 +0900

    Make a Binary Search Tree.

---
+++ Data_Structure-Algorithm/19. Binary Search Tree/main.cpp
@@ -0,0 +1,48 @@
+// C언어로 구현한 이진 탐색 트리 (BST = BinarySearchTree)
+
+#include "BinarySearchTree.h"
+
+int main(int argc, char* argv[])
+{
+	// 노드 생성
+	BSTNode* Tree = BST_CreateNode(123);
+	BSTNode* Node = NULL;
+
+	// 트리에 노드 추가
+	BST_InsertNode(Tree, BST_CreateNode(22));
+	BST_InsertNode(Tree, BST_CreateNode(9918));
+	BST_InsertNode(Tree, BST_CreateNode(424));
+	BST_InsertNode(Tree, BST_CreateNode(17));
+	BST_InsertNode(Tree, BST_CreateNode(3));
+
+	BST_InsertNode(Tree, BST_CreateNode(98));
+	BST_InsertNode(Tree, BST_CreateNode(34));
+
+	BST_InsertNode(Tree, BST_CreateNode(760));
+	BST_InsertNode(Tree, BST_CreateNode(317));
+	BST_InsertNode(Tree, BST_CreateNode(1));
+
+	// 트리 출력
+	BST_InorderPrintTree(Tree);
+	printf("\n");
+
+	// 특정 노드 삭제
+	printf("Removing 98...\n");
+
+	Node = BST_RemoveNode(Tree, NULL, 98);
+	BST_DestroyNode(Node);
+
+	BST_InorderPrintTree(Tree);
+	printf("\n");
+
+	// 새 노드 삽입
+	printf("Inserting 111...\n");
+
+	BST_InsertNode(Tree, BST_CreateNode(111));
+	BST_InorderPrintTree(Tree);
+
+	// 트리 소멸시키기
+	BST_DestroyTree(Tree);
+
+	return 0;
+}
